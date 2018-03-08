ea01ff84097464368837b65dcf37a7d260f9b7e3
jakub <gryglos@gmail.com>
2018-03-08 10:08:18 +0100

    Epizode10

--- src/main/java/com/gryglos/Main.java
+++ src/main/java/com/gryglos/Main.java
@@ -3,12 +3,23 @@ package com.gryglos;
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
+import java.math.BigDecimal;
 
 public class Main {
 
     public static void main(String[] args) {
         EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("myDatabase");
         EntityManager entityManager = entityManagerFactory.createEntityManager();
+
+        Employee employee = new Employee();
+        employee.setFirstName("Jakub");
+        employee.setLastName("Grygiel");
+        employee.setSalary(3333.3);
+        employee.setSalary2(new BigDecimal("12345.3"));
+
+        entityManager.getTransaction().begin();
+        entityManager.persist(employee);
+        entityManager.getTransaction().commit();
 
         entityManager.close();
         entityManagerFactory.close();
