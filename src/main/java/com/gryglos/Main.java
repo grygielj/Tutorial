96312a67a481c1520899260d2e95ef2f4dca6e94
jakub <gryglos@gmail.com>
2018-03-12 16:25:39 +0100

    Epizode20

--- src/main/java/com/gryglos/Main.java
+++ src/main/java/com/gryglos/Main.java
@@ -16,57 +16,45 @@ public class Main {
 
         addEmployees();
 
-        TypedQuery<Employee> query = entityManager.createQuery("select e from Pracownicy e where e.salary> :minSalary",Employee.class);
-        query.setParameter("minSalary",5000.0);
-        for(Employee employee: query.getResultList()){
-            System.out.println(employee.getFirstName());
-            System.out.println(employee.getLastName());
-            System.out.println(employee.getSalary());
-            System.out.println();
-        }
+        Query firstQuery = entityManager
+                .createQuery("select avg(e.salary), min(e.salary), max(e.salary), sum(e.salary), count(e) from Pracownicy e");
 
-        TypedQuery<Employee> secondQuery = entityManager.createQuery("select e from Pracownicy e where e.salary > ?1 and e.salary < ?2",Employee.class);
-        secondQuery.setParameter(1,2000.0);
-        secondQuery.setParameter(2,3000.0);
-        for(Employee employee: secondQuery.getResultList()){
-            System.out.println(employee.getFirstName());
-            System.out.println(employee.getLastName());
-            System.out.println(employee.getSalary());
-            System.out.println();
-        }
+        Object[] result = (Object[]) firstQuery.getSingleResult();
+        System.out.println("Srednia : "+result[0]);
+        System.out.println("Min : "+result[1]);
+        System.out.println("Max : "+result[2]);
+        System.out.println("Suma : "+result[3]);
+        System.out.println("Pracownikow : "+result[4]);
 
-        TypedQuery<Employee> thirdQuery = entityManager.createQuery("select e from Pracownicy e where e.lastName in :names",Employee.class);
-        List<String> names = new ArrayList<>();
-        names.add("Mateusiak");
-        names.add("Bednarek");
-        thirdQuery.setParameter("names",names);
-
-        for(Employee employee: thirdQuery.getResultList()){
-            System.out.println(employee.getFirstName());
-            System.out.println(employee.getLastName());
-            System.out.println(employee.getSalary());
-            System.out.println();
-        }
-
+        Query secondQuery = entityManager
+                .createQuery("select substring(e.firstName,1,3),trim(e.lastName), lower(e.firstName), upper(e.firstName),length(e.firstName)" +
+                        " from Pracownicy e where e.firstName = 'Karol'");
+        Object[] secondResult =(Object[]) secondQuery.getSingleResult();
+        System.out.println("Pierwsze trzy litery imienia "+secondResult[0]);
+        System.out.println("Nazwisko: |"+secondResult[1]+"|");
+        System.out.println("Imię małymi "+secondResult[2]);
+        System.out.println("Imię dużymi "+secondResult[3]);
+        System.out.println("Długość imienia: "+secondResult[4]);
 
         entityManager.close();
         entityManagerFactory.close();
     }
 
-    public static void addEmployees(){
-        addEmployee("Karol","Mateusiak",2633);
-        addEmployee("Karol","Bednarek",2345);
-        addEmployee("Jan ","Mateusiak",7346);
-        addEmployee("Daria","Kowalska",2352);
-        addEmployee("Monika","Ucińska",4263);
-        addEmployee("Ernest","Pająk",2634);
-        addEmployee("Kamil","Stępień",2345);
-        addEmployee("Przemek","Maciejewski",5433);
-        addEmployee("Robert","Woźniak",3324);
-        addEmployee("Agnieszka","Nowak",2808);
-        addEmployee("Angelika","Bednarska",1000);
+    public static void addEmployees() {
+        addEmployee("Karol", "   Mateusiak   ", 2633);
+        addEmployee("Marika", "Bednarek", 2345);
+        addEmployee("Jan ", "Mateusiak", 7346);
+        addEmployee("Daria", "Kowalska", 2352);
+        addEmployee("Monika", "Ucińska", 4263);
+        addEmployee("Ernest", "Pająk", 2634);
+        addEmployee("Kamil", "Stępień", 2345);
+        addEmployee("Przemek", "Maciejewski", 5433);
+        addEmployee("Robert", "Woźniak", 3324);
+        addEmployee("Agnieszka", "Nowak", 2808);
+        addEmployee("Angelika", "Bednarska", 1000);
     }
-    public static void addEmployee(String firstName, String lastName, double salary){
+
+    public static void addEmployee(String firstName, String lastName, double salary) {
         Employee employee = new Employee();
         employee.setFirstName(firstName);
         employee.setLastName(lastName);
